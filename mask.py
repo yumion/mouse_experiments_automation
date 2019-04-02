93a71f0c29ce2526fada008412eb7e0608803aa7
yumion <yumion28@gmail.com>
2019-04-02 22:11:52 +0900

    fix mask

--- mask.py
+++ mask.py
@@ -16,3 +16,16 @@ plt.show()
 # binary scale
 plt.imshow(binary, 'gray')
 plt.show()
+
+
+img = cv2.medianBlur(img, 5)
+img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
