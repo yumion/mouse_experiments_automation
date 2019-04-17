3340db8f37ea85ac3ff7e1f62e48868fa3208a20
yumion <a.yamada0703@gmail.com>
2019-04-17 20:41:13 +0900

    複数の動画を1つのcsvでannotaion保存。train/testで分ける

--- make_annotation_csv.py
+++ make_annotation_csv.py
@@ -1,27 +1,31 @@
 import pandas as pd
+import numpy as np
 from glob import glob
 import os
 import shutil
 
 # パスを指定
-# csv_dir_path = 'raw_data/digtime_over3sec'
-# frame_dir_path = 'raw_data/convert/frames'
+csv_dir_path = 'raw_data/digtime_over3sec'
+frame_dir_path = 'raw_data/frames'
+# 保存ファイル名
+savename = 'annotation_all_over3sec'
+# データ数のtrainに使う割合
+split_rate = 0.9
 
 
-# label_csvs = glob(csv_dir_path+'/*')
-# label_csvs[0]
+# 保存ファイル生成
+with open(savename+'train.csv', 'w') as f:
+    f.write('frame,class\n')
+with open(savename+'test.csv', 'w') as f:
+    f.write('frame,class\n')
 
-# 教師ラベル
-df = pd.read_csv(label_csv)
-start_time = df['start'].to_list()
-end_time = df['end'].to_list()
+# 全サンプルのdigtime.csv読み込み
+label_csvs = glob(csv_dir_path+'/*')
+np.random.shuffle(label_csvs)
+# train/testに分ける
+label_csvs_train = label_csvs[:int(len(label_csvs)*split_rate)]
+label_csvs_test = label_csvs[int(len(label_csvs)*split_rate):]
 
-# 入力データ
-# label_csv_basename = os.path.basename(label_csvs[0]).split('.')[0]
-# frames_file = glob('*_'+label_csv_basename+'*/frames/*')
-
-frames_file = sorted(frames_file)
-frames_file
 def time_to_sec(time):
     '''時刻を秒数へ変換'''
     if ':' in time: # excel表記
@@ -32,22 +36,40 @@ def time_to_sec(time):
     sec = min*60 + sec
     return sec
 
-# 秒へ変換
-start_time = [time_to_sec(t) for t in start_time] # 教師ラベル
-end_time = [time_to_sec(t) for t in end_time]
-frames_time = [os.path.basename(f) for f in frames_file] # ファイル名取得
-frames_time = [os.path.splitext(f)[0] for f in frames_time] # 拡張子を取り除く
-frames_time = [time_to_sec(f) for f in frames_time] # 入力データ
+def annotation_from_csv(label_csv, savename): # trainとtestに同じ作業をするので関数化
+    '''フレームにラベリングする'''
+    # 対象フレーム区間(ラベル"1")
+    df = pd.read_csv(label_csv)
+    start_time = df['start'].to_list()
+    end_time = df['end'].to_list()
 
-# 穴掘り時間のフレームをラベリング
-with open('annotation_NS38.csv', 'w') as f:
-    f.write('frame,class\n')
-    for i, ft in enumerate(frames_time): # まずフレームを固定して
-        cls_idx = '0'
-        for st, et in zip(start_time, end_time): # 穴掘り時間に該当するか探してラベリング
-            if st <= ft and et >= ft: # 穴掘りしている時間
-                cls_idx = '1'
+    # 入力データ
+    label_csv_basename = os.path.basename(label_csv).split('.')[0]
+    frames_file = glob(os.path.join(frame_dir_path, label_csv_basename, 'frames/*'))
+    frames_file = sorted(frames_file) # 時系列順にソート
+
+    # 秒へ変換
+    start_time = [time_to_sec(t) for t in start_time] # ラベル"1"
+    end_time = [time_to_sec(t) for t in end_time]
+    frames_time = [os.path.basename(f) for f in frames_file] # ファイル名取得
+    frames_time = [os.path.splitext(f)[0] for f in frames_time] # 拡張子を取り除く
+    frames_time = [time_to_sec(f) for f in frames_time] # 入力データ
+
+    # 穴掘り時間のフレームをラベリング
+    with open(savename, 'a') as f:
+        for i, ft in enumerate(frames_time): # まずフレームを固定して
+            cls_idx = '0'
+            for st, et in zip(start_time, end_time): # 穴掘り時間に該当するか探してラベリング
+                if st <= ft and et >= ft: # 穴掘りしている時間
+                    cls_idx = '1'
+                    f.write(frames_file[i]+','+cls_idx+'\n')
+                    break # 穴掘りを見つけたら次のフレームの検索へ
+            if cls_idx == '0': # 該当しなかったら
                 f.write(frames_file[i]+','+cls_idx+'\n')
-                break # 穴掘りを見つけたら次のフレームの検索へ
-        if cls_idx == '0': # 該当しなかったら
-            f.write(frames_file[i]+','+cls_idx+'\n')
+
+# train
+for label_csv in label_csvs_train:
+    annotation_from_csv(label_csv, savename+'train.csv')
+# test
+for label_csv in label_csvs_test:
+    annotation_from_csv(label_csv, savename+'train.csv')
